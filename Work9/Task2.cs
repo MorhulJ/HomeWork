a80e92cbace18843598b4a85d6afe2a50865b15c
Jaroslav <jaroslav.morhul@gmail.com>
2025-12-25 12:08:59 +0200

    Add files via upload

---
+++ Work9/Task2.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp;
+
+public class Task2
+{
+    public static void ShowTime()
+    {
+        Console.WriteLine("Current time: " + DateTime.Now.ToLongTimeString());
+    }
+    
+    public static void ShowDate()
+    {
+        Console.WriteLine("Current date: " + DateTime.Now.ToShortDateString());
+    }
+    
+    public static void ShowDayOfWeek()
+    {
+        Console.WriteLine("Day of week: " + DateTime.Now.DayOfWeek);
+    }
+    
+    public static double TriangleArea(double a, double h)
+    {
+        return 0.5 * a * h;
+    }
+    
+    public static double RectangleArea(double a, double b)
+    {
+        return a * b;
+    }
+}
